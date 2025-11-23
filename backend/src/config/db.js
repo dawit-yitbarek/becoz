0ebeda8151d037d480b33a3d7e18c2afc9b72022
dawit-yitbarek <dawityit17@gmail.com>
2025-11-23 13:16:39 +0300

    fix the start value in scripts section at package.json

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -6,9 +6,9 @@ const { Pool } = pkg;
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  // ssl: {
-  //   rejectUnauthorized: false,
-  // },
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 export default pool;
