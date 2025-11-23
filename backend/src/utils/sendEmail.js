98756cbe09a2d48b902d489e33f828df29a63ce6
dawit-yitbarek <dawityit17@gmail.com>
2025-11-23 14:19:46 +0300

    Change the email sending transporter

--- backend/src/utils/sendEmail.js
+++ backend/src/utils/sendEmail.js
@@ -4,10 +4,12 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: "smtp-relay.brevo.com",
+    port: 587,
+    secure: false,
     auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASSWORD,
+        user: process.env.BREVO_USER,
+        pass: process.env.BREVO_SMTP_KEY
     },
 });
 
